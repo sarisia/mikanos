93940959ec19ff1c6a220a0888054e60b8864449
Sarisia <sarisiaice@gmail.com>
2021-03-29 17:29:57 +0000

    day2a

---
+++ init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+workspace=/workspaces/mikanos
+edk2=/home/vscode/edk2
+current=$(pwd)
+
+export CONF_PATH=${workspace}/edk2/Conf
+
+ln -sf ${workspace}/edk2/MikanLoaderPkg ${edk2}
+
+cd ${edk2}
+source edksetup.sh
+cd ${current}
+
+unset -v workspace edk2 current

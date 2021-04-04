94f1656e8e0d7f1c742791d45d3dbf44fac80434
Sarisia <sarisiaice@gmail.com>
2021-04-04 10:52:01 +0000

    move code to src

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ current=$(pwd)
 
 export CONF_PATH=${workspace}/edk2/Conf
 
-ln -sf ${workspace}/MikanLoaderPkg ${edk2}
+ln -sf ${workspace}/src/MikanLoaderPkg ${edk2}
 
 cd ${edk2}
 source edksetup.sh

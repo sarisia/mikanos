5600eb93bb3a610401158ee62a176ba690a2612d
Sarisia <sarisiaice@gmail.com>
2021-05-04 15:36:55 +0000

    desktop styling

--- src/kernel/graphics.hpp
+++ src/kernel/graphics.hpp
@@ -2,6 +2,18 @@
 
 #include "frame_buffer_config.hpp"
 
+template <typename T>
+struct Vector2D {
+    T x, y;
+
+    template <typename U>
+    Vector2D<T>& operator +=(const Vector2D<U>& rhs) {
+        x += rhs.x;
+        y += rhs.y;
+        return this;
+    }
+};
+
 struct PixelColor {
     uint8_t r, g, b;
 };
@@ -36,3 +48,25 @@ public:
 
     void Write(int x, int y, const PixelColor& c) override;
 };
+
+void FillRectangle(PixelWriter& writer, const Vector2D<int>& pos,
+                   const Vector2D<int>& size, const PixelColor& color) {
+    for (int dy = 0; dy < size.y; ++dy) {
+        for (int dx = 0; dx < size.x; ++dx) {
+            writer.Write(pos.x+dx, pos.y+dy, color);
+        }
+    }
+}
+
+void DrawRectangle(PixelWriter& writer, const Vector2D<int>& pos,
+                   const Vector2D<int>& size, const PixelColor& color) {
+    for (int dx = 0; dx < size.x; ++dx) {
+        writer.Write(pos.x+dx, pos.y, color);
+        writer.Write(pos.x+dx, pos.y+size.y-1, color);
+    }
+
+    for (int dy = 0; dy < size.y; ++dy) {
+        writer.Write(pos.x, pos.y+dy, color);
+        writer.Write(pos.x+size.x-1, pos.y+dy, color);
+    }
+}
--- src/kernel/main.cpp
+++ src/kernel/main.cpp
@@ -7,6 +7,35 @@
 #include "font.hpp"
 #include "console.hpp"
 
+const int kMouseCursorWidth = 15;
+const int kMouseCursorHeight = 24;
+const char mouse_cursor_shape[kMouseCursorHeight][kMouseCursorWidth+1] = {
+    "@              ",
+    "@@             ",
+    "@.@            ",
+    "@..@           ",
+    "@...@          ",
+    "@....@         ",
+    "@.....@        ",
+    "@......@       ",
+    "@.......@      ",
+    "@........@     ",
+    "@.........@    ",
+    "@..........@   ",
+    "@...........@  ",
+    "@............@ ",
+    "@......@@@@@@@@",
+    "@......@       ",
+    "@....@@.@      ",
+    "@...@ @.@      ",
+    "@..@   @.@     ",
+    "@.@    @.@     ",
+    "@@      @.@    ",
+    "@       @.@    ",
+    "         @.@   ",
+    "         @@@   "
+};
+
 // memory allocator
 
 void* operator new(size_t size, void* buf) {
@@ -48,22 +77,33 @@ void KernelMain(
         break;
     }
 
-    for (int x = 0; x < frame_buffer_config.horizontal_resolution; ++x) {
-        for (int y = 0; y < frame_buffer_config.vertical_resolution; ++y) {
-            pixel_writer->Write(x, y, { 255, 255, 255 });
-        }
-    }
+    const int kFrameWidth = frame_buffer_config.horizontal_resolution;
+    const int kFrameHeight = frame_buffer_config.vertical_resolution;
+    const PixelColor kDesktopBGColor{58, 110, 165};
+    const PixelColor kDesktopFGColor{255, 255, 255};
 
-    for (int x = 0; x < 200; ++x) {
-        for (int y = 0; y < 100; ++y) {
-            pixel_writer->Write(x, y, { 0, 255, 0 });
-        }
-    }
+    // background
+    FillRectangle(*pixel_writer, {0, 0}, {kFrameWidth, kFrameHeight-50}, kDesktopBGColor);
+    // taskbar
+    FillRectangle(*pixel_writer, {0, kFrameHeight-50}, { kFrameWidth, 50 }, {212, 208, 200});
+    // selected app
+    FillRectangle(*pixel_writer, {0, kFrameHeight-50}, {kFrameWidth/5, 50}, {80, 80, 80});
+    DrawRectangle(*pixel_writer, {10, kFrameHeight-40}, {30, 30}, {160, 160, 160});
+
 
     console = new(console_buf) Console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
     
-    for (int i = 0; i < 27; ++i) {
-        printk("printk: %d\n", i);
+    printk("Welcome to MikanOS!\n");
+
+    // draw mouse cursor
+    for (int dy = 0; dy < kMouseCursorHeight; ++dy) {
+        for (int dx = 0; dx < kMouseCursorWidth; ++dx) {
+            if (mouse_cursor_shape[dy][dx] == '@') {
+                pixel_writer->Write(200+dx, 100+dy, {0, 0, 0});
+            } else if (mouse_cursor_shape[dy][dx] == '.') {
+                pixel_writer->Write(200+dx, 100+dy, {255, 255, 255});
+            }
+        }
     }
 
     while (1) {
